2524b59d9ca4f5aa2fd00b92f579549a41c5af3f
usna <nus963@google.com>
2015-03-24 11:27:02 +0900

    spring config

--- src/main/java/webapp/App.java
+++ src/main/java/webapp/App.java
@@ -1,5 +1,7 @@
 package webapp;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+      
+        
     }
 }
