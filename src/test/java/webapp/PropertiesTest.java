276250a2acd929d6cb83d227b7f6c9e601c98a4f
usna <nus963@google.com>
2015-03-24 15:28:29 +0900

    ㄹㄹ

--- src/test/java/webapp/PropertiesTest.java
+++ src/test/java/webapp/PropertiesTest.java
@@ -1,14 +1,28 @@
 package webapp;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 
 public class PropertiesTest {
 
+	static Log log = LogFactory.getLog(PropertiesTest.class);
+	
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		
+		ClassPathResource resource = new ClassPathResource("/message.properties");
+		
+		Properties prop = new Properties();
+		
+		prop.load(resource.getInputStream());
+		
+		String value = prop.getProperty("required");
+		log.info("value =" + value);
 	}
 
 }
